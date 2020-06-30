136b1021cf907f2583bb7123a7cc6e0e21e24b51
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-30 11:09:02 -0500

    1000.1

---
+++ freeCodeCamp/reset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ ! -a website ]]
+then 
+  mkdir website
+fi
+
+find ./website -not -name '.' -not -name '..' -not -name 'website' -delete
+cp -r ./freeCodeCamp/reset_files/website/. ./website
+
+if [[ ! -a website/client/assets ]]
+then 
+  mkdir website/client/assets
+fi
